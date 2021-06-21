b7d7c7076ad96c1caffb608ae356cba58d529732
alexellevu <newnapster83@gmail.com>
2021-06-21 19:26:09 +0200

    inseriti asterischi al posto dell' input del get dell' utente

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 /*  Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. */
 
-$paragrafo = "prova stringa di testo per php e inizio back-end";
-
-
+$paragrafo = "prova di una stringa di testo per php e di inizio back-end";
+ $censura = $_GET['cens'];
+ 
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +15,23 @@ $paragrafo = "prova stringa di testo per php e inizio back-end";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    h1{
+      color:green;  
+    }
+    </style>
+  
 </head>
 <body>
-   <p><?php echo $paragrafo ?></p>
+   <h1><?php echo $paragrafo ?></h1>
    <?php var_dump($paragrafo) ?>
    <h3>la lunghezza della stringa è: <?php echo strlen($paragrafo) ?></h3>
+    <h1>la nuova stringa censurata sarà:</h1>
+    <h2><?php echo str_replace($censura,'***',$paragrafo)?> di lunghezza: <?php echo strlen($paragrafo) ?></h2>
+
+    
+
+    
+   <h4></h4>
 </body>
 </html>
