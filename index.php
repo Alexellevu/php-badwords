3b8699051d5187cfd46f34e5523bc8041d8a5c44
alexellevu <newnapster83@gmail.com>
2021-06-21 16:34:48 +0200

    creata variabile con paragrafo stampata sull' html con la sua lunghezza

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+/*  Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. */
+
+$paragrafo = "prova stringa di testo per php e inizio back-end";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +17,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <p><?php echo $paragrafo ?></p>
+   <?php var_dump($paragrafo) ?>
+   <h3>la lunghezza della stringa è: <?php echo strlen($paragrafo) ?></h3>
 </body>
 </html>
